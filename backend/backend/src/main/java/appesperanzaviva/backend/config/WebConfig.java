a850490650fa81dd13fa941b21a2832ae9dda4b1
David-Q-M <davidquispemaucaylle04@gmail.com>
2026-01-18 17:49:56 -0500

    url cambiado que acepte a railway

--- backend/backend/src/main/java/appesperanzaviva/backend/config/WebConfig.java
+++ backend/backend/src/main/java/appesperanzaviva/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los controladores (Usuarios, Auditoría, Reportes)
-                .allowedOrigins("http://localhost:4200") // Permite el origen de tu Angular
+                .allowedOrigins("https://lucid-consideration-production.up.railway.app") // Permite el origen de tu Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos necesarios para los Wireframes
                 .allowedHeaders("*") // Permite todos los encabezados
                 .allowCredentials(true) // Necesario para manejar sesiones en el futuro
